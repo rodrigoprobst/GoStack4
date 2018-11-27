19391b3c8d3a7721f2f88f0bb84f544f8ed3005b
Rodrigo Becker Probst <becker.rodrigo@outlook.com>
2018-11-26 23:01:59 -0200

    migrations and file controller

--- GoNode/module2/src/routes.js
+++ GoNode/module2/src/routes.js
@@ -9,6 +9,8 @@ const guestMiddleware = require('./app/middlewares/guest')
 
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const DashboardController = require('./app/controllers/DashboardController')
+const FileController = require('./app/controllers/FileController')
 
 routes.use((req, res, next) => {
   res.locals.flashSuccess = req.flash('success')
@@ -16,7 +18,7 @@ routes.use((req, res, next) => {
   return next()
 })
 
-routes.use('/app', authMiddleware)
+routes.get('/files/:file', FileController.show)
 
 routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
@@ -24,10 +26,10 @@ routes.post('/signin', SessionController.store)
 routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
+routes.use('/app', authMiddleware)
+
 routes.get('/app/logout', SessionController.destroy)
 
-routes.get('/app/dashboard', (req, res) => {
-  return res.render('dashboard')
-})
+routes.get('/app/dashboard', DashboardController.index)
 
 module.exports = routes
