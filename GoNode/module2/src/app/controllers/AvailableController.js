f14d100dfb2a58f788f2cdc468b11f4897a1d2ae
Rodrigo Becker Probst <becker.rodrigo@outlook.com>
2018-11-27 21:01:58 -0200

    available controller fix

--- GoNode/module2/src/app/controllers/AvailableController.js
+++ GoNode/module2/src/app/controllers/AvailableController.js
@@ -18,10 +18,28 @@ class AvailableController {
       }
     })
 
-    const schedule = []
-    for (let index = 8; index <= 18; index++) {
-      schedule[index - 8] = index + ':00'
-    }
+    const schedule = [
+      '08:00',
+      '08:30',
+      '09:00',
+      '09:30',
+      '10:00',
+      '10:30',
+      '11:00',
+      '11:30',
+      '13:00',
+      '13:30',
+      '14:00',
+      '14:30',
+      '15:00',
+      '15:30',
+      '16:00',
+      '16:30',
+      '17:00',
+      '17:30',
+      '18:00',
+      '18:30'
+    ]
 
     const available = schedule.map(time => {
       const [hour, minute] = time.split(':')
