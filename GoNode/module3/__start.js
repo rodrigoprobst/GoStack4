c8ba88a2470cd826555b1119442118123d16bd3a
Rodrigo Becker Probst <becker.rodrigo@outlook.com>
2018-11-28 13:00:08 -0200

    basic start

---
+++ GoNode/module3/__start.js
@@ -0,0 +1,36 @@
+// yarn add express nunjucks
+// yarn add nodemon eslint -D
+// npx eslint --init
+//   Use a popular style guide
+//   Standard (https://github.com/standard/standard)
+//   JSON
+//   Y
+// del package-lock.json
+//   yarn
+
+/** scripts no package.json
+ *
+  "scripts": {
+    "start": "nodemon index.js"
+  },
+ */
+
+/**
+       _______. _______   ______      __    __   _______  __       __   ________   _______
+    /       ||   ____| /  __  \    |  |  |  | |   ____||  |     |  | |       /  |   ____|
+   |   (----`|  |__   |  |  |  |   |  |  |  | |  |__   |  |     |  | `---/  /   |  |__
+    \   \    |   __|  |  |  |  |   |  |  |  | |   __|  |  |     |  |    /  /    |   __|
+.----)   |   |  |____ |  `--'  '--.|  `--'  | |  |____ |  `----.|  |   /  /----.|  |____
+|_______/    |_______| \_____\_____\\______/  |_______||_______||__|  /________||_______|
+
+  */
+
+// yarn add sequelize
+// yarn add sequelize-cli -D
+// npx sequelize init
+
+// POSTGRES
+// yarn add pg
+
+// MySql
+// yarn add mysql2
