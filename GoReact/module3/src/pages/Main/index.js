f703fe0ec47f71469c9fb579edd42ef36ca4c64a
Rodrigo Becker Probst <becker.rodrigo@outlook.com>
2018-12-10 11:11:20 -0200

    nova aplicação pronta

--- GoReact/module3/src/pages/Main/index.js
+++ GoReact/module3/src/pages/Main/index.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 // import styles from './styles';
 
-import TodoList from '../TodoList';
-
 export default class Main extends Component {
   state = {
     // loading: false,
   };
 
   render() {
-    return <TodoList />;
+    return <div />;
   }
 }
